ff67c9c4e8cf53a275f554147b9a4bee5e4f89cf
Filipe Gomes <99850739+filipegomes11@users.noreply.github.com>
2025-09-07 15:33:44 -0300

    Replace aws_s3_client with official AWS SDK

--- lib/services/aws_s3_service.dart
+++ lib/services/aws_s3_service.dart
@@ -1,36 +1,35 @@
 import 'dart:io';
 
-import 'package:aws_s3_client/aws_s3_client.dart';
+import 'package:aws_s3_api/s3-2006-03-01.dart' as s3;
+import 'package:aws_signature_v4/aws_signature_v4.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AwsS3Service {
-  AwsS3Service();
+  AwsS3Service()
+      : _region = dotenv.env['AWS_REGION'] ?? '',
+        _bucketName = dotenv.env['AWS_BUCKET_NAME'] ?? '' {
+    final credentials = AWSCredentials(
+      dotenv.env['AWS_ACCESS_KEY_ID'] ?? '',
+      dotenv.env['AWS_SECRET_ACCESS_KEY'] ?? '',
+    );
+    _client = s3.S3(region: _region, credentials: credentials);
+  }
+
+  late final s3.S3 _client;
+  final String _region;
+  final String _bucketName;
 
   Future<String> uploadFile(File file) async {
-    final region = dotenv.env['AWS_REGION'] ?? '';
-    final accessKey = dotenv.env['AWS_ACCESS_KEY_ID'] ?? '';
-    final secretKey = dotenv.env['AWS_SECRET_ACCESS_KEY'] ?? '';
-    final bucketName = dotenv.env['AWS_BUCKET_NAME'] ?? '';
-
-    final bucket = Bucket(
-      region: region,
-      bucketId: bucketName,
-      accessKey: accessKey,
-      secretKey: secretKey,
+    final key = file.uri.pathSegments.last;
+    final bytes = await file.readAsBytes();
+    await _client.putObject(
+      s3.PutObjectRequest(
+        bucket: _bucketName,
+        key: key,
+        body: bytes,
+        acl: s3.ObjectCannedACL.publicRead,
+      ),
     );
-
-    final fileName = file.uri.pathSegments.last;
-
-    final response = await bucket.uploadFile(
-      fileName,
-      file,
-      'public-read',
-    );
-
-    if (response.statusCode == 200) {
-      return 'https://$bucketName.s3.$region.amazonaws.com/$fileName';
-    } else {
-      throw Exception('Failed to upload file: ${response.statusCode}');
-    }
+    return 'https://$_bucketName.s3.$_region.amazonaws.com/$key';
   }
 }
