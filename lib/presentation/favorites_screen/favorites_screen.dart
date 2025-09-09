2006e91e496f4ea864122e45e9a106764a8d1d20
Felipe Mendes <felipe.mssilva@ufpe.br>
2025-09-08 22:12:24 -0300

    feature/fix-duplicate-report-bug-in-hamburger-menu-from-favorites-screen

--- lib/presentation/favorites_screen/favorites_screen.dart
+++ lib/presentation/favorites_screen/favorites_screen.dart
@@ -549,13 +549,6 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
                             onTap: _handleDenounce,
                           ),
                           _buildDivider(),
-                          
-                          // Denúncia
-                          _buildMenuItem(
-                            'Denúncia',
-                            onTap: _handleDenounce,
-                          ),
-                          _buildDivider(),
                         ],
                       ),
                     ),
